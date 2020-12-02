f41019da495b343539ece886225a393db3c10a8c
Alex <1343845235@qq.com>
2020-12-02 15:38:56 +0800

    2020/12/2

--- vue-admin-template-master/src/views/edu/subject/list.vue
+++ vue-admin-template-master/src/views/edu/subject/list.vue
@@ -41,7 +41,7 @@ export default {
       },
       filterNode(value, data) {
         if (!value) return true
-        return data.title.indexOf(value) !== -1
+        return data.title.toLowerCase().indexOf(value.toLowerCase()) !== -1
       }
     }
 }
