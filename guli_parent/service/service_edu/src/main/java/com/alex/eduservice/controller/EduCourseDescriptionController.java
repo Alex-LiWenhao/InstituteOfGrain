8a0ae9760fbb53e47f4dc855be00d478de59226f
Alex <1343845235@qq.com>
2020-12-20 16:26:49 +0800

    2020/12/20

---
+++ guli_parent/service/service_edu/src/main/java/com/alex/eduservice/controller/EduCourseDescriptionController.java
@@ -0,0 +1,21 @@
+package com.alex.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 课程简介 前端控制器
+ * </p>
+ *
+ * @author Alex
+ * @since 2020-12-10
+ */
+@RestController
+@RequestMapping("/eduservice/edu-course-description")
+public class EduCourseDescriptionController {
+
+}
+
