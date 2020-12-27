29ed330d04f8f3a085210a34951593c44452ae75
Alex <1343845235@qq.com>
2020-12-27 14:34:22 +0800

    2020/12/27

---
+++ common/service-base/src/main/java/com/alex/servicebase/AlexException.java
@@ -0,0 +1,27 @@
+package com.alex.servicebase;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @ClassName AlexException
+ * @Description TODO :自定义异常
+ * @Author Alex
+ * @Date 2020/11/10 23:20
+ * @Version 1.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlexException extends RuntimeException {
+    private Integer code;
+    private String message;
+    @Override
+    public String toString() {
+        return "AlexException{" +
+                "message=" + this.getMessage() +
+                ", code=" + code +
+                '}';
+    }
+}
